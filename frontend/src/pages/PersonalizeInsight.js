190fa962a96986e258d68d68a84d79bccedaaca1
Tran Duc Anh <tranducanh@sace.edu.vn>
2024-11-06 20:27:10 +1100

    make the shadow of all containers consistent on the dashboard page

--- frontend/src/pages/PersonalizeInsight.js
+++ frontend/src/pages/PersonalizeInsight.js
@@ -33,7 +33,7 @@ const PersonalizeInsightContent = () => {
         <section className="mx-auto max-w-[1920px] w-full flex flex-col gap-8 px-5 mb-10">
             <section className="grid grid-cols-1 md:grid-cols-[270px_1fr] gap-8">
                 <PredictionResult result={result} />
-                <div className="bg-white rounded-lg shadow p-4 h-[500px]">
+                <div className="bg-white rounded-lg shadow-md p-4 h-[500px]">
                     <h3 className="text-h3-sm sm:text-h3 font-semibold mb-4">Property Cost in Different States</h3>
                     <div className='h-[calc(100%-2.5rem)] sm:h-[calc(100%-3rem)]'>
                         <Choropleth 
@@ -44,7 +44,7 @@ const PersonalizeInsightContent = () => {
                 </div>
             </section>
             <section className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                <div className="flex-1 min-w-[290px] bg-white rounded-lg shadow p-4 h-[540px]">
+                <div className="flex-1 min-w-[290px] bg-white rounded-lg shadow-md p-4 h-[540px]">
                     <h3 className="text-h3-sm sm:text-h3 font-semibold mb-4">{`Property Cost in Different Cities of ${selectedState}`}</h3>
                     <div className='h-[calc(100%-3rem-41px)] sm:h-[calc(100%-4rem-41px)]'>
                         <BarChart
@@ -53,7 +53,7 @@ const PersonalizeInsightContent = () => {
                         />
                     </div>
                 </div>
-                <div className="flex-1 min-w-[290px] bg-white rounded-lg shadow p-4 h-[540px]">
+                <div className="flex-1 min-w-[290px] bg-white rounded-lg shadow-md p-4 h-[540px]">
                     <h3 className="text-h3-sm sm:text-h3 font-semibold mb-4">Area Impact</h3>
                     <div className='h-[calc(100%-3rem-41px)] sm:h-[calc(100%-4rem-41px)]'>
                         <LineChart
